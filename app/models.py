6f60555ec0325438ca3ceba7e18c53440ef382d3
Tarun Chaudhry <tarunc92@gmail.com>
2013-10-17 12:55:49 -0700

    models

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from app import app, db
+from datetime import datetime
+
+NotificationModel = db['notifications']
+UserModel = db['users']
+
+class Notification(object):
+    @staticmethod
+    def createModel(user_id, message):
+        return {
+            'message': message,
+            'created_at': datetime.utcnow(),
+            'uid': str(user_id)
+        }
+
+    @staticmethod
+    def deleteNotification(user_id, notification_id):
+        NotificationModel.update({ '_id': notification_id, 'uid': user_id }, { '$set': { 'read': True } }, upsert=False, multi=False)
+
+    @staticmethod
+    def getForUser(user):
+        return NotificationModel.find({ 'uid': user, '$or': [{ 'read': False }, { 'read': { '$exists': False } }] }, fields=['message', '_id'])
+
+    @staticmethod
+    def createNotifications(message, user_id):
+        if user_id:
+            notification = Notification.createModel(user_id, message)
+        else:
+            users = User.getUsers()
+            notification = [Notification.createModel(user['_id'], message) for user in users]
+
+        return NotificationModel.insert(notification)
+
+class User(object):
+    @staticmethod
+    def getUsers():
+        return UserModel.find(fields=['email', '_id'])
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,10 @@
 from app import app, db
 from app.crossdomain import crossdomain
+from app.models import Notification, User
 from flask import request
-from datetime import datetime
 from bson import json_util
 from bson.objectid import ObjectId
 import simplejson as json
-
-Notification = db['notifications']
-User = db['users']
-
-def createNotification(user_id, message):
-    return {
-        'message': message,
-        'created_at': datetime.utcnow(),
-        'uid': str(user_id)
-    }
-
-def deleteNotification(user_id, notification_id):
-    Notification.update({ '_id': notification_id, 'uid': user_id }, { '$set': { 'read': True } }, upsert=False, multi=False)
-
-def getUsers():
-    return User.find(fields=['email', '_id'])
 
 @app.route('/notifications', methods=['GET'])
 @crossdomain(origin=app.config.get('CORS_DOMAIN'))
@@ -29,12 +13,10 @@ def getNotifications():
     email = request.headers.get('x-balanced-email')
     admin = request.headers.get('x-balanced-admin')
 
-    print email, user, admin
-
     if not user:
         return '', 401
 
-    notification_cursor = Notification.find({ 'uid': user, '$or': [{ 'read': False }, { 'read': { '$exists': False } }] }, fields=['message', '_id'])
+    notification_cursor = Notification.getForUser(user)
     return json.dumps({ 'data': [{ 'message': doc['message'], 'id': str(doc['_id']) } for doc in notification_cursor] }, default=json_util.default)
 
 
@@ -52,26 +34,9 @@ def createNotifications():
     message = str(request.form['message'])
     user_id = request.form.get('uid')
 
-    if user_id:
-        notification = createNotification(user_id, message)
-    else:
-        users = getUsers()
-        notification = [createNotification(user['_id'], message) for user in users]
-
-    notification_id = Notification.insert(notification)
+    notification_id = Notification.createNotifications(message, user_id)
 
     return json.dumps({ 'data': str(notification_id) if isinstance(notification_id, ObjectId) else [str(obj_id) for obj_id in notification_id] }, default=json_util.default)
-
-
-@app.route('/users', methods=['GET'])
-@crossdomain(origin=app.config.get('CORS_DOMAIN'))
-def getAllUsers():
-    admin = request.headers.get('x-balanced-admin')
-
-    if not admin:
-        return 'Authorization Required', 401
-
-    return json.dumps({ 'data': [{ 'id': str(doc['_id']), 'email': doc['email'] } for doc in getUsers()] }, default=json_util.default)
 
 
 @app.route('/notification/<string:notification_id>', methods=['DELETE'])
@@ -82,7 +47,18 @@ def markNotificationsAsRead(notification_id):
     if not user:
         return '', 401
 
-    deleteNotification(user, notification_id)
+    Notification.deleteNotification(user, notification_id)
 
     return 'ok'
+
+
+@app.route('/users', methods=['GET'])
+@crossdomain(origin=app.config.get('CORS_DOMAIN'))
+def getAllUsers():
+    admin = request.headers.get('x-balanced-admin')
+
+    if not admin:
+        return 'Authorization Required', 401
+
+    return json.dumps({ 'data': [{ 'id': str(doc['_id']), 'email': doc['email'] } for doc in User.getUsers()] }, default=json_util.default)
 
